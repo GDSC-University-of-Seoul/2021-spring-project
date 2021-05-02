c89210c480c81997e24ef315f0b58531d0844d5f
rxdcxdrnine <50660684+rxdcxdrnine@users.noreply.github.com>
2021-05-02 14:54:04 +0900

    modify with eslint/prettier

--- app/BE/app.js
+++ app/BE/app.js
@@ -1,25 +1,25 @@
-require("dotenv").config()
+import express from "express";
+import { sequelize } from "./database/models/index";
 
-const awsIot = require("aws-iot-device-sdk");
-const express = require("express");
-const mqtt = require("mqtt");
-const models = require("./database/models/index.js")
+require("dotenv").config();
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
-models.sequelize.sync().then(() => {
+sequelize
+  .sync()
+  .then(() => {
     console.log("DB 연결 성공");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log("DB 연결 실패");
-    console.log("err");
-})
+    console.log(err);
+  });
 
-//express
-app.get('/', (req, res) => {
-    res.send("Connnected.");
-})
+app.get("/", (req, res) => {
+  res.send("Connnected.");
+});
 
 app.listen(app.get("port"), () => {
-    console.log(app.get("port"), "빈 포트에서 대기중.");
-})
+  console.log(app.get("port"), "빈 포트에서 대기중.");
+});
