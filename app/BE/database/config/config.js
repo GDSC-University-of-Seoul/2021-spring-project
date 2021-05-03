d368d7604bc90f8fe95881a027a06888afdc6545
rxdcxdrnine <50660684+rxdcxdrnine@users.noreply.github.com>
2021-05-03 23:23:11 +0900

    hide database name with .env

--- app/BE/database/config/config.js
+++ app/BE/database/config/config.js
@@ -4,20 +4,20 @@ export const development = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   host: process.env.DATABASE_HOST,
-  database: "kidskeeper_dev",
+  database: process.env.DATABASE_NAME_DEV,
   dialect: "postgres",
 };
 export const test = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   host: process.env.DATABASE_HOST,
-  database: "kidskeeper_test",
+  database: process.env.DATABASE_NAME_TEST,
   dialect: "postgres",
 };
 export const production = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   host: process.env.DATABASE_HOST,
-  database: "kidskeeper_prod",
+  database: process.env.DATABASE_NAME_PROD,
   dialect: "postgres",
 };
