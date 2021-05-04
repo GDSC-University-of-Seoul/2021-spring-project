d993661ed7dbce88b56aa4f126b6d564bd244801
handal95 <45425838+handal95@users.noreply.github.com>
2021-05-05 03:33:11 +0900

    add search in sub directory func

--- app/ML/parser.py
+++ app/ML/parser.py
@@ -11,7 +11,6 @@ def check_file(file):
         return file
     else:
         files = glob.glob("./**/" + file, recursive=True)  # find file
-
         FILE_NOT_FOUND_MSG = f"File Not Found: {file}"
         MULTIPLE_FILE_MSG = f"Multiple files match '{file}', specify exact path:{files}"
 
@@ -42,10 +41,36 @@ def parse_children_node(root):
 
 
 def parse_xml(file: Path):
+    """
+    Parse the XML files in a tree structure.
+    """
     check_file(file)  # Check the existency of the file
 
     doc = ET.parse(file)
     data = parse_children_node(doc.getroot())
+    return data
 
 
-parse_xml("E:/dataset/10-1/10-1_cam01_assault03_place07_night_spring.xml")
+def search_sub_directory(directory: Path, recursive=True, extension=None):
+    """
+    Find the target path
+    """
+    assert Path(directory).exists(), "Path is not existence"
+    assert Path(directory).is_dir(), "Path should be direcory"
+
+    target_file = directory + "/**"
+    if extension:
+        target_file = target_file + "/*." + extension
+
+    files = glob.glob(target_file, recursive=recursive)
+    assert len(files), "No files is not founded : "
+    return files
+
+
+if __name__ == "__main__":
+    dirpath = "FILEPATH"  # config로 대체 예정
+    files = search_sub_directory(dirpath, extension="xml")
+
+    data = list()
+    for file in files:
+        data.append(parse_xml(file))
