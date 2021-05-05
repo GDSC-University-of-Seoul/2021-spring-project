6daa694c0755f7e1e88e94796c0ba4dda84d66c1
handal95 <45425838+handal95@users.noreply.github.com>
2021-05-05 17:26:53 +0900

    add comment

--- app/detect-model/train.py
+++ app/detect-model/train.py
@@ -5,18 +5,23 @@ from utils.files import search_file
 
 
 def train(opt):
+    """
+    Train the model
+    """
+    # Stage 0. Load the Model Setting 
     with open(opt.data) as f:
-        data_dict = yaml.safe_load(f)
+        config = yaml.safe_load(f)
 
-    dirpath = data_dict["train"]
-    files = search_file(dirpath, extension="xml")
+    # Search xml file in directory
+    files = search_file(config["train"], extension="xml")  
 
-    data = list()
+    metadata = list()
     for file in files:
-        data.append(parse_xml(file))
+        metadata.append(parse_xml(file))
 
 
 if __name__ == "__main__":
+    # Argument options
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--data", type=str, default="data/model.yml", help="model.yml path"
