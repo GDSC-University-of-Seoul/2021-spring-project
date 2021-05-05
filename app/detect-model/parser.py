9f64d0930b1a0e76a56fdac2415841a54346f9ac
handal95 <45425838+handal95@users.noreply.github.com>
2021-05-05 16:49:23 +0900

    clean up utility codes

--- app/detect-model/parser.py
+++ app/detect-model/parser.py
@@ -1,25 +1,9 @@
-import glob
 import xml.etree.ElementTree as ET
 from pathlib import Path
+from utils.files import check_file, search_file
 
 
-def check_file(file):
-    """
-    Search for file if not found
-    """
-    if Path(file).is_file() or file == "":
-        return file
-    else:
-        files = glob.glob("./**/" + file, recursive=True)  # find file
-        FILE_NOT_FOUND_MSG = f"File Not Found: {file}"
-        MULTIPLE_FILE_MSG = f"Multiple files match '{file}', specify exact path:{files}"
-
-        assert len(files), FILE_NOT_FOUND_MSG  # assert file was found
-        assert len(files) == 1, MULTIPLE_FILE_MSG  # assert unique
-        return files[0]  # return file
-
-
-def parse_children_node(root):
+def parse_xml_tree(root):
     """
     Parse the children of xml tree node
     """
@@ -33,9 +17,9 @@ def parse_children_node(root):
         if len(item) > 0:
             if data.get(item.tag) is None:
                 data[item.tag] = list()
-            data[item.tag].append(parse_children_node(item))
+            data[item.tag].append(parse_xml_tree(item))
         else:
-            data[item.tag] = parse_children_node(item)
+            data[item.tag] = parse_xml_tree(item)
 
     return data
 
@@ -47,29 +31,13 @@ def parse_xml(file: Path):
     check_file(file)  # Check the existency of the file
 
     doc = ET.parse(file)
-    data = parse_children_node(doc.getroot())
+    data = parse_xml_tree(doc.getroot())
     return data
-
-
-def search_sub_directory(directory: Path, recursive=True, extension=None):
-    """
-    Find the target path
-    """
-    assert Path(directory).exists(), "Path is not existence"
-    assert Path(directory).is_dir(), "Path should be direcory"
-
-    target_file = directory + "/**"
-    if extension:
-        target_file = target_file + "/*." + extension
-
-    files = glob.glob(target_file, recursive=recursive)
-    assert len(files), "No files is not founded : "
-    return files
 
 
 if __name__ == "__main__":
     dirpath = "FILEPATH"  # config로 대체 예정
-    files = search_sub_directory(dirpath, extension="xml")
+    files = search_file(dirpath, extension="xml")
 
     data = list()
     for file in files:
