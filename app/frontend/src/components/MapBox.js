a776092bd04b586c02af6a263b1e97c886c09990
Taehee_Lee <tlfla01@gmail.com>
2021-05-11 22:11:17 +0900

    #KDSK-58 setting area color style by childhouse data

--- app/frontend/src/components/MapBox.js
+++ app/frontend/src/components/MapBox.js
@@ -3,6 +3,7 @@ import React, { useCallback, useEffect, useMemo, useState } from "react";
 import ReactMapGL, { Popup, Layer, Source, Marker } from "react-map-gl";
 import { areaLayer, highlightLayer } from "../utils/mapbox/mapStyle";
 import xmlParser from "../utils/mapbox/xmlParser";
+import MapBoxCategory from "./MapBoxCategory";
 
 const MapboxAccessToken = process.env.REACT_APP_MAPBOX;
 
@@ -18,29 +19,35 @@ function MapBox({ data }) {
     return { data };
   }, [data]);
 
-  // map 구역 hover 기능
+  // map 구역 hover 기능 - hover 중인 지역구 표시
   const [hoverInfo, setHoverInfo] = useState(null);
 
+  // Todo : 지역구 어린이집 사건·사고에 대한 레이블 정보 추가
   const hoverHandler = useCallback((e) => {
     const hoverArea = e.features && e.features[0];
     setHoverInfo({
       longitude: e.lngLat[0],
       latitude: e.lngLat[1],
       areaName: hoverArea && hoverArea.properties.sggnm,
+      childHouseCnt: hoverArea && hoverArea.properties.childHouseCnt,
     });
   }, []);
   const selectedArea = (hoverInfo && hoverInfo.areaName) || "";
   const filter = useMemo(() => ["in", "sggnm", selectedArea], [selectedArea]);
 
-  // map 구역 click 기능
+  // map 구역 click 기능 - 지역구 내에 있는 어린이집 데이터를 통한 설정
   const [childHouseInfo, setChildHouseInfo] = useState(null);
 
   const clickHandler = useCallback(async () => {
-    // Todo : selectedArea를 시군구 코드로 변환 후 그에 맞춰 DB에서 fetch
+    // Todo : selectedArea를 시군구 코드로 변환 후 그에 맞춰 DB에서 어린이집 데이터 fetch
+    const area = {
+      은평구: "Eunpyeonggu",
+      종로구: "Jongnogu",
+    };
     try {
-      if (selectedArea === "은평구") {
+      if (selectedArea === "은평구" || selectedArea === "종로구") {
         const fetchChildHouse = await axios.get(
-          "/src/assets/data/Eunpyeonggu_childhouse_data.xml"
+          `/src/assets/data/${area[selectedArea]}_childhouse_data.xml`
         );
         xmlParser(fetchChildHouse.data).then((data) => {
           const childHouse = data.response.item.filter(
@@ -57,6 +64,7 @@ function MapBox({ data }) {
   }, [selectedArea]);
 
   return (
+    <>
     <ReactMapGL
       {...viewport}
       mapStyle="mapbox://styles/mapbox/streets-v11"
@@ -69,6 +77,7 @@ function MapBox({ data }) {
         <Layer {...areaLayer} />
         <Layer {...highlightLayer} filter={filter} />
       </Source>
+      {/* Todo : 툴팁 메세지 정보 - 지역구, 사건·사고 유형, 발생건수 추가 */}
       {selectedArea && (
         <Popup
           longitude={hoverInfo.longitude}
@@ -76,9 +85,11 @@ function MapBox({ data }) {
           closeButton={false}
           className="popup"
         >
-          {selectedArea}
+          <h1>{selectedArea}</h1>
+          <div>어린이집 개수 : {hoverInfo.childHouseCnt}</div>
         </Popup>
       )}
+      {/* Todo : 마커 표시 - 사건·사고가 발생한 어린이집의 경우 다른 색상의 마커로 표시 */}
       {childHouseInfo &&
         childHouseInfo.map((childHouse, index) => (
           <Marker
@@ -88,7 +99,8 @@ function MapBox({ data }) {
             className="marker"
           />
         ))}
-    </ReactMapGL>
+    </ReactMapGL>    
+    </>
   );
 }
 export default MapBox;
--- app/frontend/src/utils/mapbox/mapStyle.js
+++ app/frontend/src/utils/mapbox/mapStyle.js
@@ -1,19 +1,40 @@
+// 카테고리 스타일
+export const categoryStyle = [
+  [0, "#e74c3c"],
+  [50, "#e67e22"],
+  [100, "#2ecc71"],
+  [150, "#1abc9c"],
+  [200, "#3498db"],
+  [250, "#9b59b6"],
+  [300, "#bdc3c7"],
+  [350, "#95a5a6"],
+  [400, "#34495e"],
+];
+
+// 기본 렌더링 스타일
 export const areaLayer = {
   id: "area",
   type: "fill",
   paint: {
     "fill-outline-color": "#d0ff00",
-    "fill-color": "#04e40f",
-    "fill-opacity": 0.8,
+    "fill-color": {
+      property: "childHouseCnt",
+      stops: categoryStyle
+    },
+    "fill-opacity": 0.5,
   },
 };
 
+// hover 시 적용되는 스타일
 export const highlightLayer = {
   id: "area-highlighted",
   type: "fill",
   paint: {
     "fill-outline-color": "#d0ff00",
-    "fill-color": "#0082ff",
+    "fill-color": {
+      property: "childHouseCnt",
+      stops: categoryStyle,
+    },
     "fill-opacity": 0.8,
   },
 };
