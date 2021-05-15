11daaa9e8e934e3104544618dd03e323fd25322a
handal95 <45425838+handal95@users.noreply.github.com>
2021-05-15 16:16:42 +0900

    minor error fix

--- app/detect-model/utils/memory.py
+++ app/detect-model/utils/memory.py
@@ -1,13 +1,13 @@
 import time
 import psutil
 
-pid = input("Input the target process id")
+pid = int(input("Input the target process id : "))
 py = psutil.Process(pid)
 
 while True:
     try:
-        memoryUse = py.memory_info()[0]/2.**30  # memory use in GB...I think
-        print(f'memory use: {memoryUse:.4f} GB')
+        memoryUse = py.memory_info()[0] / 2.0 ** 30  # memory use in GB...I think
+        print(f"memory use: {memoryUse:.4f} GB")
         time.sleep(1)
     except KeyboardInterrupt:
         break
