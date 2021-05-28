a86fce12e5c434b8634e862ac8929b8996f4b41f
owao <46257761+owao@users.noreply.github.com>
2021-05-28 16:52:22 +0900

    wifi header

--- device/camera-store/camera-store.ino
+++ device/camera-store/camera-store.ino
@@ -1,26 +1,4 @@
-//#define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
-#include "esp_log.h"
-#include "esp_http_server.h"
-#include "esp_camera.h"
-
-//#include <WiFi.h>   // redundant
-
-#include "ESP32FTPServer.h"
-#include <HTTPClient.h>
-
-// set #define FTP_DEBUG in ESP32FtpServer.h to see ftp verbose on serial
-FtpServer ftpSrv; 
-
-// Time
-#include "time.h"
-#include "lwip/err.h"
-#include "lwip/apps/sntp.h"
-
-// MicroSD
-#include "driver/sdmmc_host.h"
-#include "driver/sdmmc_defs.h"
-#include "sdmmc_cmd.h"
-#include "esp_vfs_fat.h"
+#include "camerawifi.h";
 
 long current_millis;
 long last_capture_millis = 0;
@@ -67,13 +45,6 @@ int overtime_count = 0;
 
 // GLOBALS
 #define BUFFSIZE 512
-
-//
-// EDIT ssid and password
-// 연결할 와이파이 아이디&패스워드
-// zzz
-const char *ssid = "cafeyeoneuil_0";
-const char *password = "24680111";
 
 // these are just declarations -- look below to edit defaults
 
@@ -152,9 +123,6 @@ char the_page[3000];
 
 char localip[20];
 WiFiEventId_t eventID;
-
-#include "soc/soc.h"
-#include "soc/rtc_cntl_reg.h"
 
 void setup() {
   //WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector  // creates other problems
@@ -310,43 +278,6 @@ void major_fail()
     digitalWrite(33, HIGH);
     delay(500);
   }
-}
-
-//wifi 설정 함수
-bool init_wifi()
-{
-  int connAttempts = 0;
-
-  // zzz
-  // Set your Static IP address
-  // 본인 IP 주소 설정 가능
-  IPAddress local_IP(192, 168, 1, 222);
-
-  // Set your Gateway IP address
-  IPAddress gateway(192, 168, 1, 254);
-
-  IPAddress subnet(255, 255, 0, 0);
-  IPAddress primaryDNS(8, 8, 8, 8);   // optional
-  IPAddress secondaryDNS(8, 8, 4, 4); // optional
-
-  WiFi.setHostname("ESP32CAM-222"); // does not seem to do anything with my wifi router ???
-
-  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS))
-  {
-    Serial.println("STA Failed to configure");
-    major_fail();
-  }
-
-  WiFi.begin(ssid, password); //위에서 입력한 ID와 패스워드로 동작
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-    if (connAttempts > 10)
-      return false;
-    connAttempts++;
-  }
-  return true;
 }
 
 void init_time()
