5b6c16add47b7d47ef5ce0afc7a2adc374b2d72e
YubeenSon <s49451392@gmail.com>
2021-06-02 17:10:45 +0900

    install babel for eslint

--- app/DB/config/config.js
+++ app/DB/config/config.js
@@ -1,23 +1,33 @@
-require("dotenv").config();
+"use strict";
 
-export const development = {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.production = exports.test = exports.development = void 0;
+
+require('dotenv').config();
+
+var development = {
   database: process.env.DATABASE_NAME_DEV,
   username: process.env.DATABASE_USERNAME_DEV,
   password: process.env.DATABASE_PASSWORD_DEV,
   host: process.env.DATABASE_HOST_DEV,
-  dialect: "postgres",
+  dialect: 'postgres'
 };
-export const test = {
+exports.development = development;
+var test = {
   database: process.env.DATABASE_NAME_TEST,
   username: process.env.DATABASE_USERNAME_TEST,
   password: process.env.DATABASE_PASSWORD_TEST,
   host: process.env.DATABASE_HOST_TEST,
-  dialect: "postgres",
+  dialect: 'postgres'
 };
-export const production = {
+exports.test = test;
+var production = {
   database: process.env.DATABASE_NAME_PROD,
   username: process.env.DATABASE_USERNAME_PROD,
   password: process.env.DATABASE_PASSWORD_PROD,
   host: process.env.DATABASE_HOST_PROD,
-  dialect: "postgres",
+  dialect: 'postgres'
 };
+exports.production = production;
