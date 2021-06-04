c50e25a495967a3d7142ff2d8659516854ef21ef
handal95 <45425838+handal95@users.noreply.github.com>
2021-06-04 16:03:50 +0900

    move file directory

--- libs/alphapose/alphapose/version.py
+++
@@ -1,5 +0,0 @@
-# GENERATED VERSION FILE
-# TIME: Fri Jun  4 01:06:52 2021
-
-__version__ = '0.3.0+cbc364f'
-short_version = '0.3.0'
---
+++ models/libs/alphapose/scripts/test.sh
@@ -0,0 +1,8 @@
+python scripts/demo_inference.py 
+    --cfg ./configs/coco/resnet/256x192_res50_lr1e-3_1x.yaml 
+    --checkpoint pretrained_models/fast_res50_256x192.pth 
+    --indir examples/demo 
+    --outdir examples/res
+    --video C:/Users/Handal/github/2021-spring-project/models/data/videoplayback.mp4 
+    --vis
+    --pose_track
