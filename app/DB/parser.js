9dc358b348cbe57027b40e13d27f296232892a54
YubeenSon <s49451392@gmail.com>
2021-06-12 21:39:22 +0900

    combine 3 array to 1

--- app/DB/parser.js
+++ app/DB/parser.js
@@ -1,37 +1,31 @@
 let json = require('./region_code_name.json');
-// console.log(json.존재[1100000000]); // 서울특별시
 
 var len = Object.keys(json.존재).length;
 var keys = Object.keys(json.존재);
 
-var district_code = [];
-var district_name = [];
-var parent_code = [];
+var districts = [];
 
 for (let i = 0; i < len; i++) {
-  var address = json.존재[keys[i]]; // type : string
+  var address = json.존재[keys[i]];
   var addressSplit = address.split(' ');
 
   if (addressSplit.length == 1) {
-    district_name.push(address);
-    district_code.push(keys[i]);
-    parent_code.push(null);
-    // console.log(address);
-    // console.log(keys[i]);
+    districts.push({
+      code: keys[i],
+      name: address,
+      parent_code: null,
+    });
   } else if (addressSplit.length == 2) {
     if (addressSplit[0] == '세종특별자치시') {
       continue;
     }
     var parentCode = keys[i].slice(0, 2) + '00000000';
-    district_name.push(addressSplit[1]);
-    district_code.push(keys[i]);
-    parent_code.push(parentCode);
-    // console.log(addressSplit[1]);
-    // console.log(keys[i]);
-    // console.log(parentCode);
+    districts.push({
+      code: keys[i],
+      name: addressSplit[1],
+      parent_code: parentCode,
+    });
   }
 }
 
-exports.district_code = district_code;
-exports.district_name = district_name;
-exports.parent_code = parent_code;
+exports.districts = districts;
