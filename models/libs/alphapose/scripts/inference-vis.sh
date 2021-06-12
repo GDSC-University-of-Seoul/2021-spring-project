ba59ca1bec45b230feeba00d3296b82a624a4ff3
handal95 <45425838+handal95@users.noreply.github.com>
2021-06-12 21:22:22 +0900

    add script

---
+++ models/libs/alphapose/scripts/inference-vis.sh
@@ -0,0 +1,1 @@
+python scripts/demo_inference.py --cfg ./configs/coco/resnet/256x192_res50_lr1e-3_1x.yaml --checkpoint pretrained_models/fast_res50_256x192.pth --video resize2.mp4 --outdir examples/res --vis --pose_track
--- models/libs/alphapose/scripts/inference.sh
+++ models/libs/alphapose/scripts/inference.sh
@@ -1,8 +1,8 @@
 set -x
 
-CONFIG=$1
-CKPT=$2
-VIDEO=$3
+CONFIG=${1:-"./configs/coco/resnet/256x192_res50_lr1e-3_1x.yaml"}
+CKPT=${2:-"./pretrained_models/fast_res50_256x192.pth"}
+VIDEO=${3:"./examples/demo/resized2.mp4"}
 OUTDIR=${4:-"./examples/res"}
 
 python scripts/demo_inference.py \
@@ -10,4 +10,4 @@ python scripts/demo_inference.py \
     --checkpoint ${CKPT} \
     --video ${VIDEO} \
     --outdir ${OUTDIR} \
-    --detector yolo  --save_img --save_video
+    --detector yolo  --save_img --save_video --vis
