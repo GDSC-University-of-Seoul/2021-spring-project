79c770578cf1454b979ab9f103417ed15fae79ce
handal95 <45425838+handal95@users.noreply.github.com>
2021-06-19 04:31:53 +0900

    unused yolo code remove

--- models/libs/alphapose/alphapose/datasets/__init__.py
+++ models/libs/alphapose/alphapose/datasets/__init__.py
@@ -2,13 +2,11 @@ from .coco_det import Mscoco_det
 from .concat_dataset import ConcatDataset
 from .custom import CustomDataset
 from .mscoco import Mscoco
-from .mpii import Mpii
 
 __all__ = [
     "CustomDataset",
     "Mscoco",
     "Mscoco_det",
-    "Mpii",
     "ConcatDataset",
     "coco_wholebody",
     "coco_wholebody_det",
