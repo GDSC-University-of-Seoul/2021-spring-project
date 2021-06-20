b6f257aa3e34da322531e3167c55ab8a4036d89b
edit8080 <leetaehee0205@gmail.com>
2021-06-20 16:03:45 +0900

    #KDSK-83 create cctvs menu

--- app/frontend/src/pages/Cctvs.js
+++ app/frontend/src/pages/Cctvs.js
@@ -1,3 +1,10 @@
+import {
+  AiOutlineMinusSquare,
+  AiOutlinePlusSquare,
+  AiOutlineRetweet,
+} from "react-icons/ai";
+
+import Button from "@material-ui/core/Button";
 import CctvTableContainer from "../containers/CctvTableContainer";
 import React from "react";
 
@@ -8,9 +15,56 @@ import React from "react";
  */
 
 function Cctvs() {
+  const createHandler = () => {};
+  const updateHandler = () => {};
+  const deleteHandler = () => {};
+
+  const menuHandler = (e) => {
+    const select = e.target.dataset.id;
+
+    switch (select) {
+      case "create":
+        createHandler();
+        break;
+      case "update":
+        updateHandler();
+        break;
+      case "delete":
+        deleteHandler();
+        break;
+      default:
+        return;
+    }
+  };
+
   return (
     <>
       <section className="section cctvs">
+        <div className="cctvs-menu" onClick={menuHandler}>
+          <Button
+            data-id="create"
+            variant="outlined"
+            startIcon={<AiOutlinePlusSquare />}
+          >
+            추가
+          </Button>
+          <Button
+            data-id="update"
+            variant="outlined"
+            color="primary"
+            startIcon={<AiOutlineRetweet />}
+          >
+            변경
+          </Button>
+          <Button
+            data-id="delete"
+            variant="outlined"
+            color="secondary"
+            startIcon={<AiOutlineMinusSquare />}
+          >
+            삭제
+          </Button>
+        </div>
         <div className="container cctvs-section">
           <CctvTableContainer />
         </div>
