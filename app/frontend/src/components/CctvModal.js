0d6880678262c01d20d3a2807dc813d2b79abfda
Taehee_Lee <leetaehee0205@gmail.com>
2021-06-23 21:42:27 +0900

    #KDSK-83 add mac address validation check

--- app/frontend/src/components/CctvModal.js
+++ app/frontend/src/components/CctvModal.js
@@ -1,25 +1,47 @@
+import React, { useState } from "react";
+
 import Button from "@material-ui/core/Button";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
-import React from "react";
 import Select from "@material-ui/core/Select";
 import TextField from "@material-ui/core/TextField";
 
 function CctvModal({ isOpen, setIsOpen }) {
   const qualityItems = ["SD", "HD", "FHD", "QHD", "UHD"];
+  const [macValid, setMacValid] = useState(true);
+
+  const changeMacInput = (e, macAddress) => {
+    const macRegex = new RegExp(`^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$`);
+
+    macRegex.test(macAddress) ? setMacValid(true) : setMacValid(false);
+  };
+
+  const submitCctvForm = (e) => {
+    e.preventDefault();
+
+    const targets = e.target;
+    let formInfo = {};
+
+    for (let target of targets) {
+      if (target.name) formInfo[target.name] = target.value;
+    }
+    setIsOpen(false);
+  };
+
   const closeModal = () => {
     setIsOpen(false);
   };
+
   return (
     <>
       {isOpen && (
-        <div className="cctvModal" onClick={closeModal}>
-          <form className="cctvModal-form" onClick={(e) => e.stopPropagation()}>
+        <div className="cctvModal">
+          <form className="cctvModal-form" onSubmit={submitCctvForm}>
             <div className="cctvModal-title">어린이집 CCTV 정보 추가/변경</div>
             <TextField
               label="어린이집 명"
-              data-id="center_name"
+              name="center_name"
               required
               InputLabelProps={{
                 shrink: true,
@@ -27,7 +49,7 @@ function CctvModal({ isOpen, setIsOpen }) {
             />
             <TextField
               label="어린이집 주소"
-              data-id="center_address"
+              name="center_address"
               required
               InputLabelProps={{
                 shrink: true,
@@ -35,7 +57,7 @@ function CctvModal({ isOpen, setIsOpen }) {
             />
             <TextField
               label="설치 장소"
-              data-id="cctv_name"
+              name="cctv_name"
               required
               InputLabelProps={{
                 shrink: true,
@@ -43,17 +65,24 @@ function CctvModal({ isOpen, setIsOpen }) {
             />
             <TextField
               label="MAC 주소"
-              data-id="cctv_mac"
+              name="cctv_mac"
               required
+              placeholder="예) 12-34-56-78-0A-BC"
               InputLabelProps={{
                 shrink: true,
               }}
+              onChange={(e) => changeMacInput(e, e.target.value)}
             />
+            {!macValid && (
+              <div className="cctv_mac-alert">
+                유효하지 않은 MAC 주소입니다.
+              </div>
+            )}
             <FormControl required>
               <InputLabel id="cctv_quality-label">화질</InputLabel>
               <Select
                 labelId="cctv_quality-label"
-                data-id="cctv_quality"
+                name="cctv_quality"
                 defaultValue={"SD"}
               >
                 {qualityItems.map((item, idx) => (
@@ -66,7 +95,7 @@ function CctvModal({ isOpen, setIsOpen }) {
             <TextField
               type="date"
               label="설치 일자"
-              data-id="install_date"
+              name="install_date"
               InputLabelProps={{
                 shrink: true,
               }}
@@ -74,7 +103,7 @@ function CctvModal({ isOpen, setIsOpen }) {
             <TextField
               type="date"
               label="제거 일자"
-              data-id="uninstall_date"
+              name="uninstall_date"
               InputLabelProps={{
                 shrink: true,
               }}
@@ -85,6 +114,7 @@ function CctvModal({ isOpen, setIsOpen }) {
                 variant="contained"
                 color="primary"
                 disableElevation
+                disabled={!macValid}
               >
                 확인
               </Button>
