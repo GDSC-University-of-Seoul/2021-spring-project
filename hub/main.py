27c5f6d876e6509e972b6033f90c53c785d80700
handal <handal__@naver.com>
2021-07-07 02:23:22 +0900

    move main process code

---
+++ hub/main.py
@@ -0,0 +1,12 @@
+from utils.general import check_git_status, check_requirements
+from utils.logger import Logger
+
+def main():
+    logger = Logger().get_logger()
+    logger.info("Main Process")
+
+
+if __name__ == "__main__":
+    # check_git_status()    # Current Git Error
+    check_requirements()
+    main()
