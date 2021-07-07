ed76cf24f204d68f8ea64e11b76369a932c6db28
YubeenSon <s49451392@gmail.com>
2021-07-07 13:58:43 +0900

    delete comment

--- app/DB/app.js
+++ app/DB/app.js
@@ -3,7 +3,6 @@ const db = require('./models/transform/index.js');
 
 const sequelize = db.sequelize;
 
-// force: true -> alter: true
 (async () => {
   await sequelize.sync({ alter: true });
 })();
