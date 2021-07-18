33df06ef25c70e25eda39b1abe54b5bfdda848e9
owao <46257761+owao@users.noreply.github.com>
2021-07-18 17:44:59 +0900

    modify data form

--- hub/models/api.py
+++ hub/models/api.py
@@ -39,14 +39,27 @@ async def run_model(dirpath, filepath):
     score = abs(random.normalvariate(mu=0, sigma=0.2))
     anomal = True if score >= THRESHOLD else False
     output = {
-        "video_id": f"{dirpath}_{filepath}",
-        "start_time": "2021-07-15-12:07:00" if anomal else None,
-        "end_time": "2021-07-15-12:07:30" if anomal else None,
-        "anomaly_type": "ASSERT" if anomal else None,
-        "follow_up": None,
+        "video": {
+            "record_date": "2021-07-17 21:00:00",
+            "cctv_mac": "125454545460",
+            "storage_name": "data/cctv1/"
+        },
+        "anomaly_type": "폭행" if anomal else None,
+        "start_time": "2021-07-17 00:00:00" if anomal else None,
+        "end_time": "2021-07-17 01:00:00" if anomal else None,
     }
     ## Done MODEL
     print(f"            {filepath} anomaly score is : {score * 100:.2f} %")
 
     await asyncio.sleep(2)
     return (anomal, output)
+
+
+
+'''
+"video_id": f"{dirpath}_{filepath}",
+        "start_time": "2021-07-15-12:07:00" if anomal else None,
+        "end_time": "2021-07-15-12:07:30" if anomal else None,
+        "anomaly_type": "ASSERT" if anomal else None,
+        "follow_up": None,
+'''
