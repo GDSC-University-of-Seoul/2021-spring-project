d48bf7da6a5927700214055202f9ab06a5118688
rxdcxdrnine <rkckr1@naver.com>
2021-07-21 21:45:45 +0900

    Modify district sql query with pivot

--- server/backend/routes/district.js
+++ server/backend/routes/district.js
@@ -9,21 +9,51 @@ router.get("/", async (req, res, next) => {
     const { parent_code } = req.query;
     if (parent_code) {
       const districts = await sequelize.query(
-        `SELECT d.*, COUNT(a.anomaly_log_id)
+        `SELECT district_code,
+        district_name,
+        COALESCE(swoon_count, 0) as swoon_count,
+        COALESCE(total_count, 0) as total_count,
+        COALESCE(assault_count, 0) as assault_count
+        FROM CROSSTAB (
+        'SELECT d.district_code,
+          d.district_name,
+          COALESCE(a.anomaly_type, ''전체'') as anomaly_type,
+          COUNT(DISTINCT a.anomaly_log_id) as count
         FROM district as d
         LEFT OUTER JOIN child_care_center as c
         ON d.district_code = c.district_code
         LEFT OUTER JOIN anomaly_log as a
         ON c.center_id = a.center_id
-        WHERE d.parent_code = '${parent_code}'
-        GROUP BY d.district_code
-        ORDER BY 1`,
+        WHERE d.parent_code = ''${parent_code}''
+        GROUP BY d.district_code,
+          d.district_name,
+          ROLLUP(a.anomaly_type)
+        ORDER BY d.district_code,
+          anomaly_type',
+        'SELECT DISTINCT anomaly_type FROM anomaly_log
+        UNION
+        SELECT ''전체'' FROM anomaly_log
+        ORDER BY anomaly_type')
+        AS (district_code text,
+          district_name text,
+          swoon_count integer,
+          total_count integer,
+          assault_count integer);`,
         { type: sequelize.QueryTypes.SELECT }
       );
       res.json(districts);
     } else {
       const districts = await sequelize.query(
-        `SELECT upper_d.*, COUNT(a.anomaly_log_id)
+        `SELECT district_code, 
+        district_name,
+        COALESCE(swoon_count, 0) as swoon_count,
+        COALESCE(total_count, 0) as total_count,
+        COALESCE(assault_count, 0) as assault_count
+        FROM CROSSTAB (
+        'SELECT upper_d.district_code, 
+          upper_d.district_name, 
+          COALESCE(a.anomaly_type, ''전체'') as anomaly_type, 
+          COUNT(DISTINCT a.anomaly_log_id) as count
         FROM district as upper_d
         LEFT OUTER JOIN district as lower_d
         ON upper_d.district_code = lower_d.parent_code
@@ -32,8 +62,20 @@ router.get("/", async (req, res, next) => {
         LEFT OUTER JOIN anomaly_log as a
         ON c.center_id = a.center_id
         WHERE upper_d.parent_code IS NULL
-        GROUP BY upper_d.district_code
-        ORDER BY 1`,
+        GROUP BY upper_d.district_code,
+          upper_d.district_name,
+          ROLLUP(a.anomaly_type)
+        ORDER BY upper_d.district_code, 
+          anomaly_type',
+        'SELECT DISTINCT anomaly_type FROM anomaly_log
+        UNION
+        SELECT ''전체'' FROM anomaly_log
+        ORDER BY anomaly_type')
+        AS (district_code text, 
+          district_name text,
+          swoon_count integer,
+          total_count integer,
+          assault_count integer);`,
         { type: sequelize.QueryTypes.SELECT }
       );
       res.json(districts);
@@ -48,7 +90,16 @@ router.get("/:district_code(\\d+)", async (req, res, next) => {
   try {
     if (parseInt(req.params.district_code.slice(2, 4)) === 0) {
       const districts = await sequelize.query(
-        `SELECT upper_d.*, COUNT(a.anomaly_log_id)
+        `SELECT district_code, 
+        district_name,
+        COALESCE(swoon_count, 0) as swoon_count,
+        COALESCE(total_count, 0) as total_count,
+        COALESCE(assault_count, 0) as assault_count
+        FROM CROSSTAB (
+        'SELECT upper_d.district_code, 
+          upper_d.district_name, 
+          COALESCE(a.anomaly_type, ''전체'') as anomaly_type, 
+          COUNT(DISTINCT a.anomaly_log_id) as count
         FROM district as upper_d
         LEFT OUTER JOIN district as lower_d
         ON upper_d.district_code = lower_d.parent_code
@@ -56,23 +107,56 @@ router.get("/:district_code(\\d+)", async (req, res, next) => {
         ON lower_d.district_code = c.district_code
         LEFT OUTER JOIN anomaly_log as a
         ON c.center_id = a.center_id
-        WHERE upper_d.district_code = '${req.params.district_code}'
-        GROUP BY upper_d.district_code
-        ORDER BY 1`,
+        WHERE upper_d.district_code = ''${req.params.district_code}''
+        GROUP BY upper_d.district_code,
+          upper_d.district_name,
+          ROLLUP(a.anomaly_type)
+        ORDER BY upper_d.district_code, 
+          anomaly_type',
+        'SELECT DISTINCT anomaly_type FROM anomaly_log
+        UNION
+        SELECT ''전체'' FROM anomaly_log
+        ORDER BY anomaly_type')
+        AS (district_code text, 
+          district_name text,
+          swoon_count integer,
+          total_count integer,
+          assault_count integer);`,
         { type: sequelize.QueryTypes.SELECT }
       );
       res.json(districts);
     } else {
       const districts = await sequelize.query(
-        `SELECT d.*, COUNT(a.anomaly_log_id)
+        `SELECT district_code, 
+        district_name,
+        COALESCE(swoon_count, 0) as swoon_count,
+        COALESCE(total_count, 0) as total_count,
+        COALESCE(assault_count, 0) as assault_count
+        FROM CROSSTAB (
+        'SELECT d.district_code, 
+          d.district_name, 
+          COALESCE(a.anomaly_type, ''전체'') as anomaly_type, 
+          COUNT(DISTINCT a.anomaly_log_id) as count
         FROM district as d
         LEFT OUTER JOIN child_care_center as c
         ON d.district_code = c.district_code
         LEFT OUTER JOIN anomaly_log as a
         ON c.center_id = a.center_id
-        WHERE d.district_code = '${req.params.district_code}'
-        GROUP BY d.district_code
-        ORDER BY 1`,
+        WHERE d.district_code = ''${req.params.district_code}''
+        GROUP BY d.district_code,
+          d.district_name,
+          ROLLUP(a.anomaly_type)
+        ORDER BY d.district_code, 
+          anomaly_type',
+        'SELECT DISTINCT anomaly_type FROM anomaly_log
+        UNION
+        SELECT ''전체'' FROM anomaly_log
+        ORDER BY anomaly_type')
+        AS (district_code text, 
+          district_name text,
+          swoon_count integer,
+          total_count integer,
+          assault_count integer);`,
         { type: sequelize.QueryTypes.SELECT }
       );
       res.json(districts);
@@ -87,7 +171,16 @@ router.get("/:district_name", async (req, res, next) => {
   try {
     if (upperDistricts.includes(req.params.district_name)) {
       const districts = await sequelize.query(
-        `SELECT upper_d.*, COUNT(a.anomaly_log_id)
+        `SELECT district_code, 
+        district_name,
+        COALESCE(swoon_count, 0) as swoon_count,
+        COALESCE(total_count, 0) as total_count,
+        COALESCE(assault_count, 0) as assault_count
+        FROM CROSSTAB (
+        'SELECT upper_d.district_code, 
+          upper_d.district_name, 
+          COALESCE(a.anomaly_type, ''전체'') as anomaly_type, 
+          COUNT(DISTINCT a.anomaly_log_id) as count
         FROM district as upper_d
         LEFT OUTER JOIN district as lower_d
         ON upper_d.district_code = lower_d.parent_code
@@ -95,23 +188,56 @@ router.get("/:district_name", async (req, res, next) => {
         ON lower_d.district_code = c.district_code
         LEFT OUTER JOIN anomaly_log as a
         ON c.center_id = a.center_id
-        WHERE upper_d.district_name = '${req.params.district_name}'
-        GROUP BY upper_d.district_code
-        ORDER BY 1`,
+        WHERE upper_d.district_name = ''${req.params.district_name}''
+        GROUP BY upper_d.district_code,
+          upper_d.district_name,
+          ROLLUP(a.anomaly_type)
+        ORDER BY upper_d.district_code, 
+          anomaly_type',
+        'SELECT DISTINCT anomaly_type FROM anomaly_log
+        UNION
+        SELECT ''전체'' FROM anomaly_log
+        ORDER BY anomaly_type')
+        AS (district_code text, 
+          district_name text,
+          swoon_count integer,
+          total_count integer,
+          assault_count integer);`,
         { type: sequelize.QueryTypes.SELECT }
       );
       res.json(districts);
     } else {
       const districts = await sequelize.query(
-        `SELECT d.*, COUNT(a.anomaly_log_id)
+        `SELECT district_code, 
+        district_name,
+        COALESCE(swoon_count, 0) as swoon_count,
+        COALESCE(total_count, 0) as total_count,
+        COALESCE(assault_count, 0) as assault_count
+        FROM CROSSTAB (
+        'SELECT d.district_code, 
+          d.district_name, 
+          COALESCE(a.anomaly_type, ''전체'') as anomaly_type, 
+          COUNT(DISTINCT a.anomaly_log_id) as count
         FROM district as d
         LEFT OUTER JOIN child_care_center as c
         ON d.district_code = c.district_code
         LEFT OUTER JOIN anomaly_log as a
         ON c.center_id = a.center_id
-        WHERE d.district_name = '${req.params.district_name}'
-        GROUP BY d.district_code
-        ORDER BY 1`,
+        WHERE d.district_name = ''${req.params.district_name}''
+        GROUP BY d.district_code,
+          d.district_name,
+          ROLLUP(a.anomaly_type)
+        ORDER BY d.district_code, 
+          anomaly_type',
+        'SELECT DISTINCT anomaly_type FROM anomaly_log
+        UNION
+        SELECT ''전체'' FROM anomaly_log
+        ORDER BY anomaly_type')
+        AS (district_code text, 
+          district_name text,
+          swoon_count integer,
+          total_count integer,
+          assault_count integer);`,
         { type: sequelize.QueryTypes.SELECT }
       );
       res.json(districts);
