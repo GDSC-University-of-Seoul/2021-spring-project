f20ec3dbb1f1f521457540903770103ae1205cd3
YubeenSon <s49451392@gmail.com>
2021-07-23 02:38:33 +0900

    match timestamp format

--- server/database/seeders/20210716142704-anomaly.js
+++ server/database/seeders/20210716142704-anomaly.js
@@ -4,44 +4,44 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('anomaly', [{
       anomaly_type : '폭행',
-      start_time : '12:04:03',
-      end_time : '12:04:30',
+      start_time : '2021-07-07 12:04:03',
+      end_time : '2021-07-07 12:04:30',
       follow_up : '이상행동감지',
       video_id : '1'
     },{
       anomaly_type : '폭행',
-      start_time : '14:12:34',
-      end_time : '14:13:21',
+      start_time : '2020-09-11 14:12:34',
+      end_time : '2020-09-11 14:13:21',
       follow_up : '이상행동감지',
       video_id : '2'
     },{
       anomaly_type : '실신',
-      start_time : '10:59:58',
-      end_time : '11:02:41',
+      start_time : '2019-12-20 10:59:58',
+      end_time : '2019-12-20 11:02:41',
       follow_up : '이상행동감지',
       video_id : '3'
     },{
       anomaly_type : '실신',
-      start_time : '11:34:04',
-      end_time : '11:34:23',
+      start_time : '2020-04-05 11:34:04',
+      end_time : '2020-04-05 11:34:23',
       follow_up : '이상행동감지',
       video_id : '5'
     },{
       anomaly_type : '싸움',
-      start_time : '13:21:34',
-      end_time : '13:23:31',
+      start_time : '2019-12-21 13:21:34',
+      end_time : '2019-12-21 13:23:31',
       follow_up : '이상행동감지',
       video_id : '4'
     },{
       anomaly_type : '폭행',
-      start_time : '13:21:34',
-      end_time : '13:21:51',
+      start_time : '2019-12-21 13:21:34',
+      end_time : '2019-12-21 13:21:51',
       follow_up : '이상행동감지',
       video_id : '4'
     },{
       anomaly_type : '폭행',
-      start_time : '13:22:05',
-      end_time : '13:22:16',
+      start_time : '2019-12-21 13:22:05',
+      end_time : '2019-12-21 13:22:16',
       follow_up : '이상행동감지',
       video_id : '4'
     }])
--- server/database/seeders/20210716142948-video.js
+++ server/database/seeders/20210716142948-video.js
@@ -3,23 +3,23 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('video', [{
-      record_date : '21-07-07',
+      record_date : '2021-07-07',
       storage_name : '한화테크윈 HRX-421',
       cctv_id : '1'
     },{
-      record_date : '20-09-11',
+      record_date : '2020-09-11',
       storage_name : '한화테크윈 HRX-421',
       cctv_id : '1'
     },{
-      record_date : '19-12-20',
+      record_date : '2019-12-20',
       storage_name : '한화테크윈 HRX-421',
       cctv_id : '3'
     },{
-      record_date : '19-12-21',
+      record_date : '2019-12-21',
       storage_name : '한화테크윈 HRX-421',
       cctv_id : '7'
     },{
-      record_date : '20-04-05',
+      record_date : '2020-04-05',
       storage_name : '한화테크윈 HRX-421',
       cctv_id : '6'
     }])
