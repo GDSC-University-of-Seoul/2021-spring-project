2d925664870dd5ca9b895e8ec31701f663226576
YubeenSon <s49451392@gmail.com>
2021-07-23 14:52:58 +0900

    fit data format

--- server/database/seeders/20210716142843-member.js
+++ server/database/seeders/20210716142843-member.js
@@ -6,13 +6,13 @@ module.exports = {
       member_id : 'han31',
       password : '31han',
       member_name : '한상일',
-      member_phone : '010-1234-5678',
+      member_phone : '01012345678',
       email : 'han31@naver.com'
     },{
       member_id : 'edit8080',
       password : '0808edit',
       member_name : '이태희',
-      member_phone : '010-1357-2468',
+      member_phone : '01013572468',
       email : 'edit0808@naver.com'
     }])
   },
