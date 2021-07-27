5bee480cbb836d4b179f4f26fb0e39cc2d537a61
이태희 <leetaehee0205@gmail.com>
2021-07-27 21:27:39 +0900

    #KDSK-108 create login pages style

--- app/src/pages/Login.js
+++ app/src/pages/Login.js
@@ -1,34 +1,37 @@
 import { Button, TextField } from "@material-ui/core";
-import React, { useState } from "react";
+
+import React from "react";
 
 function Login() {
   return (
     <section className="login">
-      <div className="logo"></div>
-      <form className="login-form">
-        <TextField
-          id="outlined-helperText"
-          label="ID"
-          variant="outlined"
-          required
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="outlined"
-          required
-        />
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          disableElevation
-        >
-          로그인
-        </Button>
-      </form>
+      <div className="login-container">
+        <div className="logo"></div>
+        <form className="login-form">
+          <TextField
+            id="outlined-helperText"
+            label="ID"
+            variant="outlined"
+            required
+          />
+          <TextField
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            variant="outlined"
+            required
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disableElevation
+          >
+            로그인
+          </Button>
+        </form>
+      </div>
     </section>
   );
 }
