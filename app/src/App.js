0caadd2bcccb690dde50a89aac8d28cf99eb5400
이태희 <leetaehee0205@gmail.com>
2021-07-27 21:06:39 +0900

    #KDSK-108 create login pages

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,11 @@
+import React, { useState } from "react";
+
 import Cctvs from "./pages/Cctvs";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Logs from "./pages/Logs";
 import Monitoring from "./pages/Monitoring";
-import React from "react";
 import { Route } from "react-router-dom";
 import Settings from "./pages/Settings";
 import SideBar from "./components/SideBar";
@@ -14,15 +16,22 @@ import SideBar from "./components/SideBar";
  * @return {JSX.Element} 라우팅 컴포넌트
  */
 function App() {
+  const [login] = useState(false);
+
   return (
     <>
-      <Header />
-      <SideBar />
-      <Route path="/" component={Home} exact />
-      <Route path="/monitoring" component={Monitoring} />
-      <Route path="/cctvs" component={Cctvs} />
-      <Route path="/logs" component={Logs} />
-      <Route path="/settings" component={Settings} />
+      <Route path="/" component={Login} exact />
+      {login && (
+        <>
+          <Header />
+          <SideBar />
+          <Route path="/home" component={Home} />
+          <Route path="/monitoring" component={Monitoring} />
+          <Route path="/cctvs" component={Cctvs} />
+          <Route path="/logs" component={Logs} />
+          <Route path="/settings" component={Settings} />
+        </>
+      )}
     </>
   );
 }
