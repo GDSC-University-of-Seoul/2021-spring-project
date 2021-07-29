c2ca9ee863daa5e76a174f414deb1ebaac661862
YubeenSon <s49451392@gmail.com>
2021-07-29 21:14:11 +0900

    apply uuid id

--- server/database/seeders/20210716132608-cctv.js
+++ server/database/seeders/20210716132608-cctv.js
@@ -1,74 +1,88 @@
 'use strict';
+const { v4: uuidv4 } = require('uuid');
+
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('cctv', [{
+      cctv_id : uuidv4(),
       cctv_name : '참새반_1',
       cctv_mac : '54C4153B5737',
       quality : 'HD',
       install_date : '2015-09-01',
       center_id : '11110000143'
     },{
+      cctv_id : uuidv4(),
       cctv_name : '병아리반_1',
       cctv_mac : '1A2B3C4D5E6F',
       quality : 'HD',
       install_date : '2015-09-01',
       center_id : '11110000143'
     },{
+      cctv_id : uuidv4(),
       cctv_name : '토끼반_1',
       cctv_mac : '6A5S4D3F2G1H',
       quality : 'HD',
       install_date : '2015-09-01',
       center_id : '11110000143'
     },{
+      cctv_id : uuidv4(),
       cctv_name : '거북이반_1',
       cctv_mac : '2Q4W6E8R1T3Y',
       quality : 'HD',
       install_date : '2015-09-01',
       center_id : '11110000143'
     },{
+      cctv_id : uuidv4(),
       cctv_name : '영아1반_1',
       cctv_mac : '9H5S1I0G0B9E',
       quality : 'HD',
       install_date : '2015-07-15',
       center_id : '11110000019'
     },{
+      cctv_id : uuidv4(),
       cctv_name : '영아2반_1',
       cctv_mac : '9S8Y0B9B1A0R',
       quality : 'HD',
       install_date : '2015-07-15',
       center_id : '11110000019'
     },{
+      cctv_id : uuidv4(),
       cctv_name : '유아1반_1',
       cctv_mac : '0A1H2D3D4D5B',
       quality : 'HD',
       install_date : '2015-04-30',
       center_id : '11110000019'
     },{
+      cctv_id : uuidv4(),
       cctv_name : '유아2반_1',
       cctv_mac : '1234567890AB',
       quality : 'HD',
       install_date : '2015-04-30',
       center_id : '11110000019'
     },{
+      cctv_id : uuidv4(),
       cctv_name : '개나리반_1',
       cctv_mac : 'ZXCVBN987654',
       quality : 'HD',
       install_date : '2020-02-20',
       center_id : '11110000174'
     },{
+      cctv_id : uuidv4(),
       cctv_name : '장미반_1',
       cctv_mac : '6B5W4C3Y2Q1J',
       quality : 'HD',
       install_date : '2020-02-20',
       center_id : '11110000174'
     },{
+      cctv_id : uuidv4(),
       cctv_name : '보리수반_1',
       cctv_mac : '3Q5E8G0V2J7W',
       quality : 'HD',
       install_date : '2020-02-20',
       center_id : '11110000174'
     },{
+      cctv_id : uuidv4(),
       cctv_name : '목화반_1',
       cctv_mac : '6M8C5D0W3C7D',
       quality : 'HD',
--- server/database/seeders/20210716142704-anomaly.js
+++ server/database/seeders/20210716142704-anomaly.js
@@ -7,43 +7,43 @@ module.exports = {
       start_time : '2021-07-07 12:04:03',
       end_time : '2021-07-07 12:04:30',
       follow_up : '이상행동감지',
-      video_id : '1'
+      video_id : 'afd78c67-6be4-4e6d-b650-2d4f79028d9c'
     },{
       anomaly_type : '폭행',
       start_time : '2020-09-11 14:12:34',
       end_time : '2020-09-11 14:13:21',
       follow_up : '이상행동감지',
-      video_id : '2'
+      video_id : '4de40bdc-64e8-4970-847d-eedb9a793bd1'
     },{
       anomaly_type : '실신',
       start_time : '2019-12-20 10:59:58',
       end_time : '2019-12-20 11:02:41',
       follow_up : '이상행동감지',
-      video_id : '3'
+      video_id : 'b91dd4f6-2255-464c-9611-0ed8cc64f2bd'
     },{
       anomaly_type : '실신',
       start_time : '2020-04-05 11:34:04',
       end_time : '2020-04-05 11:34:23',
       follow_up : '이상행동감지',
-      video_id : '5'
+      video_id : 'ddee1140-ce74-4082-951a-c24b0dce6462'
     },{
       anomaly_type : '싸움',
       start_time : '2019-12-21 13:21:34',
       end_time : '2019-12-21 13:23:31',
       follow_up : '이상행동감지',
-      video_id : '4'
+      video_id : '7cf43025-c13e-4a2a-a31f-d4d5fa14f713'
     },{
       anomaly_type : '폭행',
       start_time : '2019-12-21 13:21:34',
       end_time : '2019-12-21 13:21:51',
       follow_up : '이상행동감지',
-      video_id : '4'
+      video_id : '7cf43025-c13e-4a2a-a31f-d4d5fa14f713'
     },{
       anomaly_type : '폭행',
       start_time : '2019-12-21 13:22:05',
       end_time : '2019-12-21 13:22:16',
       follow_up : '이상행동감지',
-      video_id : '4'
+      video_id : '7cf43025-c13e-4a2a-a31f-d4d5fa14f713'
     }])
   },
 
