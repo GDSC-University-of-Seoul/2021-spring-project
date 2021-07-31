f95d8b50b846809ecc125e1ba8c99a3954bf004f
handal <handal__@naver.com>
2021-07-31 17:47:01 +0900

    instance files

--- hub/utils/scheduler.py
+++ hub/utils/scheduler.py
@@ -1,37 +1,14 @@
-# cctv
-# cctv_id cctv 번호(int)    ::filename
-# area_id 설치 위치(int)
-# install_date 설치일자(date)
-# quality 화질(enum)
-# uninstall_date 제거일자(date) N
-
-# video
-# video_id 파일번호(int)    ::filename
-# record_date 생성일(date - YY/MM/DD)    ::filename
-# delete_date 파기일(date - YY/MM/DD) N
-# delete_issue 파기사유(varchar(20) N
-# storage_name 보관 저장소(int)    ::filename
-
-# anomaly_id 이상행동 식별번호(int)
-# start_time 이상행동 감지시점(timestamp - YY/MM/DD hh:mm:ss)    ::
-# end_time 이상행동 종료 파악시점(timestamp - YY/MM/DD hh:mm:ss)    ::
-# follow_up 추후조치(enum)    ::
-# video_id 관련 동영상 파일(int)
-
-import json
 import asyncio
 import requests
-from datetime import datetime
 from api import run_process
 from apscheduler.jobstores.base import JobLookupError
 from apscheduler.schedulers.background import BlockingScheduler
-
-TOTAL_DIRECTORY = 4
-
+from utils.files import dirlist
 
 class Scheduler:
     def __init__(self, config):
-        self.max_instance = str(TOTAL_DIRECTORY * 2)
+        self.total_dirs = len(dirlist(config["data"]["path"]))
+        self.max_instance = str(self.total_dirs * 2)
         self.sched = BlockingScheduler(
             {"apscheduler.job_defaults.max_instances": self.max_instance}
         )
