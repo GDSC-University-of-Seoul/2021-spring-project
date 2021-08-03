47b13b483e3a1fd6daceeffed4014285d2e7c0a9
이태희 <leetaehee0205@gmail.com>
2021-08-03 22:44:30 +0900

    #KDSK-109 apply logout modal style

--- app/src/pages/Settings.js
+++ app/src/pages/Settings.js
@@ -25,53 +25,59 @@ function Settings() {
   return (
     <>
       <section className="section settings">
-        <div>
-          <div className="container settings-section">
-            <div className="settings-title">모니터링 설정</div>
-            <hr />
-            <div className="message-control">
-              <span className="label">알림 메세지 설정</span>
-              <ToggleBtn />
-            </div>
-            <div className="alarm-control">
-              <span className="label">경고음 설정</span>
-              <ToggleBtn />
-            </div>
+        <div className="container settings-section">
+          <div className="settings-title">모니터링 설정</div>
+          <hr />
+          <div className="message-control">
+            <span className="label">알림 메세지 설정</span>
+            <ToggleBtn />
           </div>
-          <div className="container user-section">
-            <div className="settings-title">사용자 설정</div>
-            <hr />
-            <ul className="userInfo">
-              <li>
-                <span>관리자</span>
-                <input type="text" value={userName} disabled />
-              </li>
-              <li>
-                <span>계정 ID</span>{" "}
-                <input type="text" value={userId} disabled />
-              </li>
-              <li>
-                <span>관리자 이메일</span>
-                <input type="text" value={email} disabled />
-              </li>
-            </ul>
-            <div className="user-control">
-              <button className="user-modify">사용자 정보 수정</button>
-              <button className="user-logout" onClick={() => setIsLogOut(true)}>
-                로그아웃
-              </button>
-            </div>
+          <div className="alarm-control">
+            <span className="label">경고음 설정</span>
+            <ToggleBtn />
           </div>
-          {isLogOut && (
-            <Modal>
+        </div>
+        <div className="container user-section">
+          <div className="settings-title">사용자 설정</div>
+          <hr />
+          <ul className="userInfo">
+            <li>
+              <span>관리자</span>
+              <input type="text" value={userName} disabled />
+            </li>
+            <li>
+              <span>계정 ID</span> <input type="text" value={userId} disabled />
+            </li>
+            <li>
+              <span>관리자 이메일</span>
+              <input type="text" value={email} disabled />
+            </li>
+          </ul>
+          <div className="user-control">
+            <button className="user-modify">사용자 정보 수정</button>
+            <button className="user-logout" onClick={() => setIsLogOut(true)}>
+              로그아웃
+            </button>
+          </div>
+        </div>
+        {isLogOut && (
+          <Modal>
+            <div className="logout-checkModal">
               ⚠️ 로그아웃 하시겠습니까?
               <div>
-                <button onClick={logOutHandler}>확인</button>
-                <button onClick={() => setIsLogOut(false)}>취소</button>
+                <button onClick={logOutHandler} className="confirm-btn">
+                  확인
+                </button>
+                <button
+                  onClick={() => setIsLogOut(false)}
+                  className="cancel-btn"
+                >
+                  취소
+                </button>
               </div>
-            </Modal>
-          )}
-        </div>
+            </div>
+          </Modal>
+        )}
       </section>
     </>
   );
