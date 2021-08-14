e93dfeebc85ae678bd6d5b1a760c7401c53b482e
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2021-08-14 21:10:30 +0900

    add jeju seeder

---
+++ server/database/seeders/20210812093331-jeju-cctv.js
@@ -0,0 +1,61 @@
+'use strict';
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('cctv', [
+      {
+        cctv_id: uuidv4(),
+        cctv_name: '제주1반_1',
+        cctv_mac: '44C4153B5737',
+        quality: 'HD',
+        install_date: '2015-09-01',
+        center_id: '49110000558',
+      },
+      {
+        cctv_id: uuidv4(),
+        cctv_name: '제주2반_1',
+        cctv_mac: '0A2B3C4D5E6F',
+        quality: 'HD',
+        install_date: '2015-09-01',
+        center_id: '49110000606',
+      },
+      {
+        cctv_id: uuidv4(),
+        cctv_name: '제주3반_1',
+        cctv_mac: '5A564D3F2916',
+        quality: 'HD',
+        install_date: '2015-09-01',
+        center_id: '49110000224',
+      },
+      {
+        cctv_id: uuidv4(),
+        cctv_name: '서귀포반_1',
+        cctv_mac: '154D6E881337',
+        quality: 'HD',
+        install_date: '2015-09-01',
+        center_id: '49130000165',
+      },
+      {
+        cctv_id: uuidv4(),
+        cctv_name: '서귀포2반_1',
+        cctv_mac: '8A5C1D0F0B9E',
+        quality: 'HD',
+        install_date: '2015-07-15',
+        center_id: '49130000076',
+      },
+      {
+        cctv_id: uuidv4(),
+        cctv_name: '서귀포3반_1',
+        cctv_mac: '04850B9B1A07',
+        quality: 'HD',
+        install_date: '2015-07-15',
+        center_id: '49130000129',
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('cctv', null);
+  },
+};
---
+++ server/database/seeders/20210812095156-jeju-video.js
@@ -0,0 +1,49 @@
+'use strict';
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('video', [
+      {
+        video_id: uuidv4(),
+        record_date: '2021-08-12',
+        storage_name: '한화테크윈 HRX-421',
+        cctv_id: '6565cdb9-815e-4487-b370-459edafe414e',
+      },
+      {
+        video_id: uuidv4(),
+        record_date: '2021-08-12',
+        storage_name: '한화테크윈 HRX-421',
+        cctv_id: '40784ca9-5532-4a30-8ee0-e4e6ae5e490a',
+      },
+      {
+        video_id: uuidv4(),
+        record_date: '2021-08-12',
+        storage_name: '한화테크윈 HRX-421',
+        cctv_id: '15a7f2ad-20f2-4a5e-afe7-18cfcd0336b3',
+      },
+      {
+        video_id: uuidv4(),
+        record_date: '2021-08-12',
+        storage_name: '한화테크윈 HRX-421',
+        cctv_id: '2d1ace82-9d56-4431-b12a-5cc8ef2f5501',
+      },
+      {
+        video_id: uuidv4(),
+        record_date: '2021-08-12',
+        storage_name: '한화테크윈 HRX-421',
+        cctv_id: 'fb5fb955-a748-4eb3-bf8d-d1a621587050',
+      },
+      {
+        video_id: uuidv4(),
+        record_date: '2021-08-12',
+        storage_name: '한화테크윈 HRX-421',
+        cctv_id: '91efe5cf-7bda-45c9-b726-7e39bbb8413c',
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('video', null);
+  },
+};
