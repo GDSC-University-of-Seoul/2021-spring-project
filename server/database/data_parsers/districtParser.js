0b0b563205bdd1325b6b33ed39a9773aadac7383
YubeenSon <s49451392@gmail.com>
2021-08-15 23:46:50 +0900

    refacoring districtParser.js

--- server/database/data_parsers/districtParser.js
+++ server/database/data_parsers/districtParser.js
@@ -1,32 +1,32 @@
 let json = require('../../../.dummy/region_code_name.json');
 
-var len = Object.keys(json.존재).length;
-var keys = Object.keys(json.존재);
+let keys = Object.keys(json.존재);
+let districts = [];
 
-var districts = [];
+for (let i = 0; i < keys.length; i++) {
+  let code = keys[i];
 
-for (let i = 0; i < len; i++) {
-  var address = json.존재[keys[i]];
-  var addressSplit = address.split(' ');
+  let address = json.존재[code];
+  let addressSplit = address.split(' ');
 
   if (addressSplit.length == 1) {
     districts.push({
-      district_code: keys[i],
+      district_code: code,
       district_name: address,
       parent_code: null,
     });
-  } else if (addressSplit.length == 2) {
+  } 
+  else if (addressSplit.length == 2) {
     if (addressSplit[0] == '세종특별자치시') {
       continue;
     }
-    var parentCode = keys[i].slice(0, 2) + '00000000';
+    let parentCode = code.slice(0, 2) + '00000000';
     districts.push({
-      district_code: keys[i],
+      district_code: code,
       district_name: addressSplit[1],
       parent_code: parentCode,
     });
   }
 }
 
-exports.districts = districts;
 exports.districts = districts;
