72147e8ac96249f31aab935d8903e854d91eb83b
Lint Action <lint-action@samuelmeuli.com>
2021-08-17 23:01:18 +0000

    Fix code style issues with Black

--- hub/tests/test_files.py
+++ hub/tests/test_files.py
@@ -93,7 +93,9 @@ def test_searchfile_one():  # 파일이 하나만 있을 때(파일명 지정)
     __files = "C:\\Users\\user\\Documents\\GitHub\\2021-spring-project\\hub\\tests\\testdata\\testmp4.mp4"
 
     # 실행시켰을 때 결과
-    directory = "C:\\Users\\user\\Documents\\GitHub\\2021-spring-project\\hub\\tests\\testdata"
+    directory = (
+        "C:\\Users\\user\\Documents\\GitHub\\2021-spring-project\\hub\\tests\\testdata"
+    )
     filename = "testmp4.mp4"
     recursive = True
     extension = None
@@ -111,11 +113,15 @@ def test_searchfile_list():  # 파일이 여러 개 있을 때(파일명 지정)
     ]
 
     # 실행시켰을 때 결과
-    directory = "C:\\Users\\user\\Documents\\GitHub\\2021-spring-project\\hub\\tests\\testdata"
+    directory = (
+        "C:\\Users\\user\\Documents\\GitHub\\2021-spring-project\\hub\\tests\\testdata"
+    )
     filename = "test2mp4.mp4"
     recursive = True
     extension = None
     files = search_file(directory, filename, recursive, extension)
 
     # 결과 비교
-    assert __files == files, f"Couldn't read all file list about {filename} on {directory}"
+    assert (
+        __files == files
+    ), f"Couldn't read all file list about {filename} on {directory}"
