6ef1607fbd8898240d64f6bd33a6396b191eee32
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2021-08-21 18:42:04 +0900

    edit bulkDelete and record_date

--- server/database/seeders/20210716143008-log.js
+++ server/database/seeders/20210716143008-log.js
@@ -14,48 +14,48 @@ module.exports = {
         center_id: '11110000143',
         center_name: '대학로어린이집',
         address: '서울특별시 종로구 이화장길 86-22',
-        record_date: '2021-09-11',
+        record_date: '2021-07-08',
         anomaly_type: '폭행',
       },
       {
         center_id: '11110000143',
         center_name: '대학로어린이집',
         address: '서울특별시 종로구 이화장길 86-22',
-        record_date: '2021-12-20',
+        record_date: '2021-07-07',
         anomaly_type: '실신',
       },
       {
         center_id: '11110000019',
         center_name: '누상어린이집',
         address: '서울특별시 종로구 옥인길 23-4 (누상동)',
-        record_date: '2020-04-05',
+        record_date: '2021-07-07',
         anomaly_type: '실신',
       },
       {
         center_id: '11110000019',
         center_name: '누상어린이집',
         address: '서울특별시 종로구 옥인길 23-4 (누상동)',
-        record_date: '2019-12-21',
+        record_date: '2021-07-07',
         anomaly_type: '싸움',
       },
       {
         center_id: '11110000019',
         center_name: '누상어린이집',
         address: '서울특별시 종로구 옥인길 23-4 (누상동)',
-        record_date: '2019-12-21',
+        record_date: '2021-07-07',
         anomaly_type: '폭행',
       },
       {
         center_id: '11110000019',
         center_name: '누상어린이집',
         address: '서울특별시 종로구 옥인길 23-4 (누상동)',
-        record_date: '2019-12-21',
+        record_date: '2021-07-07',
         anomaly_type: '폭행',
       },
     ]);
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('anomaly_log', { center_id: ['11110000019', '11110000143'] });
+    await queryInterface.bulkDelete('anomaly_log', { record_date: ['2021-07-07', '2021-07-08'] });
   },
 };
