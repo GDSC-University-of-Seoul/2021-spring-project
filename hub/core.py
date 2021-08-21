d7bdaca7c5d73b7b90941778d842fa9cdc3e2eef
handal <handal__@naver.com>
2021-08-21 18:49:22 +0900

    update refactoring

--- hub/core.py
+++ hub/core.py
@@ -6,10 +6,12 @@ import os
 import time
 import asyncio
 import random
+from datetime import datetime, timedelta
 from clients.aws import AWSClient
 from clients.hub import HubClient
 from utils.logger import Logger
 from utils.config import Config
+from utils.time import to_file_format
 from utils.updater import check_update
 
 logger = Logger().get_logger()
@@ -60,6 +62,15 @@ class KidsKeeper(object):
 
     async def analize(self, path):
         print(f"  Run Model at {path}...")
+
+        cur_time = datetime.now()
+        ano_time = cur_time + timedelta(minutes=1, seconds=45)
+        end_time = cur_time + timedelta(minutes=5)
+        filename = os.path.join(path, to_file_format(cur_time))
+        
+        with open(f"{filename}.mp4", mode="w") as f:
+            f.write("")
+
         await asyncio.sleep(2)
         ## RUN MODEL
         score = self.scoring_func()
@@ -67,27 +78,29 @@ class KidsKeeper(object):
 
         output = {
             "video": {
-                "record_date": "2021-07-17 21:00:00",
-                "cctv_mac": "125454545460",
+                "record_date": str(cur_time),
+                "cctv_mac": "54C4153B5737",
                 "storage_name": path,
             },
             "anomaly_type": "폭행" if anomal else None,
-            "start_time": "2021-07-17 00:00:00" if anomal else None,
-            "end_time": "2021-07-17 01:00:00" if anomal else None,
+            "start_time": str(ano_time) if anomal else None,
+            "end_time": str(end_time) if anomal else None,
         }
+
         ## Done MODEL
         print(f"    {path} anomaly score is : {score * 100:.2f} %")
 
         if anomal is True:
             print(f" {path} is anomaly!!!")
-            self.aws_client.send_anomaly(data=output)
+            response = self.aws_client.send_anomaly(data=output)
+            print(" >> response ", response)
             await asyncio.sleep(2)
         return (anomal, output)
 
     def scoring_func(self):
         # TEMP CODE
         # Randomize option
-        return abs(random.normalvariate(mu=0, sigma=1))
+        return abs(random.normalvariate(mu=0, sigma=0.5))
 
     def detect_anomaly(self, score, threshold=0.7):
         if score >= threshold:
