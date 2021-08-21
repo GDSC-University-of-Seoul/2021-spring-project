c07b398e4ba1670df22899bae2fd646705baee4a
rxdcxdrnine <rkckr1@naver.com>
2021-08-22 02:05:44 +0900

    fix pagination offset/limit

--- server/backend/utils/getPagination.js
+++ server/backend/utils/getPagination.js
@@ -1,7 +1,7 @@
 export default async (listSize, page, range) => {
   const rangeSize = 10;
 
-  const currentPage = (range - 1) * rangeSize + page;
+  const currentPage = (parseInt(range) - 1) * 10 + parseInt(page);
   const offset = (currentPage - 1) * listSize;
 
   return offset;
