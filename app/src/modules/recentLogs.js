08c216a714b5c786f4e7b74bf45e17ad4c6c6843
이태희 <leetaehee0205@gmail.com>
2021-08-22 13:51:27 +0900

    #KDSK-129 add : fetch all recent data for alarm log

--- app/src/modules/recentLogs.js
+++ app/src/modules/recentLogs.js
@@ -6,6 +6,7 @@ const RECENT_LOGS_LOADING = "recentLogs/RECENT_LOGS_LOADING";
 const RECENT_LOGS_FETCH = "recentLogs/RECENT_LOGS_FETCH";
 const RECENT_LOGS_ERROR = "recentLogs/RECENT_LOGS_ERROR";
 const RECENT_LOGS_PAGINATION = "recentLogs/RECENT_LOGS_PAGINATION";
+const RECENT_LOGS_ALL_FETCH = "recentLogs/RECENT_LOGS_ALL_FETCH";
 
 // 백엔드 API 를 통해 anomaly log 데이터 Fetch
 export const fetchRecentLogs = (pagination) => async (dispatch) => {
@@ -36,11 +37,38 @@ export const fetchRecentLogs = (pagination) => async (dispatch) => {
   }
 };
 
-// 전체 로그의 현재 페이지네이션 정보 설정
+// 최근 로그의 현재 페이지네이션 정보 설정
 export const recentLogsPagination = (pagination) => ({
   type: RECENT_LOGS_PAGINATION,
   payload: pagination,
 });
+
+// 모든 최근 로그 데이터 Fetch
+export const recentLogsAllFetch = (pagination) => async (dispatch) => {
+  try {
+    const { listSize, range, page } = pagination;
+
+    let allRecentLogs = await axios.get(
+      `${process.env.REACT_APP_API_SERVER}/api/anomalies/logs/recent?list_size=${listSize}&range=${range}&page=${page}`
+    );
+
+    // 날짜 형식 변경
+    allRecentLogs.data.rows = allRecentLogs.data.rows.map((logData) => {
+      const recordDate = new Date(logData.record_date);
+
+      return {
+        ...logData,
+        record_date: `${dateUTCFormat(recordDate)} ${timeUTCFormat(
+          recordDate
+        )}`,
+      };
+    });
+
+    dispatch({ type: RECENT_LOGS_ALL_FETCH, payload: allRecentLogs.data.rows });
+  } catch (e) {
+    dispatch({ type: RECENT_LOGS_ERROR, payload: e });
+  }
+};
 
 const initialState = {
   loading: false,
@@ -51,6 +79,7 @@ const initialState = {
     page: 1,
   },
   recentLogs: [], // 페이지네이션에 대한 최근 로그 데이터
+  allRecentLogs: [], // 모든 최근 로그 데이터
   count: {
     // 전체 페이지네이션 정보
     listCount: 1,
@@ -71,7 +100,7 @@ export default function recentLogsReducer(state = initialState, action) {
       return {
         ...state,
         loading: false,
-        logsData: action.payload.rows,
+        recentLogs: action.payload.rows,
         count: action.payload.count,
         error: null,
       };
@@ -86,6 +115,13 @@ export default function recentLogsReducer(state = initialState, action) {
         ...state,
         loading: false,
         pagination: action.payload,
+        error: null,
+      };
+    case RECENT_LOGS_ALL_FETCH:
+      return {
+        ...state,
+        loading: false,
+        allRecentLogs: action.payload,
         error: null,
       };
     default:
