53310cbcb58f4f12a6f1d3cf8726956a72e46009
이태희 <leetaehee0205@gmail.com>
2021-08-24 21:25:47 +0900

    #KDSK-131 add : create search bar event handler

--- app/src/components/SearchBar.js
+++ app/src/components/SearchBar.js
@@ -1,30 +1,55 @@
 import { Button, FormControl, MenuItem, Select } from "@material-ui/core";
+import React, { useState } from "react";
 
 import { AiOutlineSearch } from "react-icons/ai";
-import React from "react";
 
-function SearchBar({ searchCategories }) {
+function SearchBar({ searchCategories, searchInfo, setSearchInfo }) {
+  const [searchType, setType] = useState();
+
+  const changeType = (e) => {
+    setType(e.target.value);
+  };
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    setSearchInfo({
+      type: searchType,
+      keyword: e.target.keyword.value,
+    });
+  };
+
   return (
     <div className="searchForm">
-      <FormControl variant="outlined">
-        <Select defaultValue={searchCategories[0].value}>
-          {searchCategories.map((category) => (
-            <MenuItem value={category.value} key={category.value}>
-              {category.text}
-            </MenuItem>
-          ))}
-        </Select>
-        <input type="text" className="searchContent" />
-        <Button
-          variant="contained"
-          color="primary"
-          disableElevation
-          type="submit"
-          startIcon={<AiOutlineSearch />}
-        >
-          검색
-        </Button>
-      </FormControl>
+      <form onSubmit={submitForm}>
+        <FormControl variant="outlined">
+          <Select
+            defaultValue={searchCategories[0].value}
+            onChange={changeType}
+          >
+            {searchCategories.map((category) => (
+              <MenuItem id="type" value={category.value} key={category.value}>
+                {category.text}
+              </MenuItem>
+            ))}
+          </Select>
+
+          <input
+            id="keyword"
+            type="text"
+            className="searchContent"
+            placeholder="검색어를 입력하세요"
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            disableElevation
+            type="submit"
+            startIcon={<AiOutlineSearch />}
+          >
+            검색
+          </Button>
+        </FormControl>
+      </form>
     </div>
   );
 }
