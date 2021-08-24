f8f86db121561cb3050378cca3454fb4cf411be5
rxdcxdrnine <rkckr1@naver.com>
2021-08-25 00:16:31 +0900

    add date compare for recent anomaly logs

--- server/backend/repositories/anomalyLog.js
+++ server/backend/repositories/anomalyLog.js
@@ -5,18 +5,27 @@ const findAllLogs = async (listSize, page, range, type, keyword, recent) => {
   const offset = await getOffset(listSize, page, range);
   const logFilter = await getLogOption(type, keyword);
 
-  if (!logFilter.record_date) {
-    const endDate = new Date();
-    let startDate = new Date();
+  const endDate = new Date();
+  let startDate = new Date();
 
+  if (recent) {
+    endDate.setHours(endDate.getHours() + 9);
+    startDate.setDate(endDate.getDate() - 1);
+  } else {
+    endDate.setHours(endDate.getHours() + 9);
+    startDate.setDate(endDate.getDate() - 60);
+  }
+
+  if (logFilter.record_date) {
     if (recent) {
-      endDate.setHours(endDate.getHours() + 9);
-      startDate.setDate(endDate.getDate() - 1);
-    } else {
-      endDate.setHours(endDate.getHours() + 9);
-      startDate.setDate(endDate.getDate() - 60);
+      const keywordDate = new Date(keyword);
+      if (keywordDate < startDate) {
+        logFilter.record_date = {
+          [Sequelize.Op.between]: [startDate, endDate],
+        };
+      }
     }
-
+  } else {
     logFilter.record_date = {
       [Sequelize.Op.between]: [startDate, endDate],
     };
