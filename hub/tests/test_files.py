4dd520f7545e8a0b36cfab0c860cbe5c7f4a84ea
owao <46257761+owao@users.noreply.github.com>
2021-08-25 23:03:47 +0900

    modify function names

--- hub/tests/test_files.py
+++ hub/tests/test_files.py
@@ -4,65 +4,65 @@ from utils.files import *
 ###
 # test dirlist
 ###
-def test_dirlist_return():
+def test_dirlist_folders_list():
     """
     test returning the subfolders list
     폴더 목록이 맞는지 확인
     """
     # 정상적으로 작동해야 하는 결과
-    __list = [
+    __dir_list = [
         ".\\testdata\\testdata2",
         ".\\testdata\\testdata3",
     ]
 
     # 실행시켰을 때 결과
     path = ".\\testdata"
-    list = dirlist(path)
+    dir_list = dirlist(path)
 
     # 결과 비교
     FOUND_WRONG_LIST_MSG = f"Wrong list of folder in {path}"
-    assert __list == list, FOUND_WRONG_LIST_MSG
+    assert __dir_list == dir_list, FOUND_WRONG_LIST_MSG
 
 
 ###
 # test check_dir
 ###
-def test_checkdir_return_true():
+def test_checkdir_exist_folder():
     """
     test returning True if there was the folder
     폴더 존재시 True 반환 확인
     """
     # 정상적으로 작동해야 하는 결과
-    __check = True
+    __dircheck = True
 
     # 실행시켰을 때 결과
     path = ".\\testdata"
-    check = check_dir(path, force=True)
+    dircheck = check_dir(path, force=True)
 
     # 결과 비교
     FILE_NOT_FOUND_MSG = f"Couldn't recognize folder on {path}"
-    assert __check == check, FILE_NOT_FOUND_MSG
+    assert __dircheck == dircheck, FILE_NOT_FOUND_MSG
 
 
-def test_checkdir_return_false():
+def test_checkdir_none_folder():
     """
     test returning False or make path if there wasn't the folder
     폴더 부재시 False or 경로 생성
     """
     # 정상적으로 작동해야 하는 결과
-    __making = True
-    __none_making = False
+    __dir_making = True
+    __dir_none_making = False
 
     # 실행시켰을 때 결과
     path = ".\\data"
-    check = check_dir(path, force=False)
+    dircheck = check_dir(path, force=False)
     check_dir(path, force=True)
 
     # 결과 비교
     FOUND_WRONG_FOLDER_MSG = f"wrong recognize folder on {path}"
     NO_MAKE_FOLDER_MSG = f"Couldn't make folder on {path}"
-    assert check == __none_making, FOUND_WRONG_FOLDER_MSG
-    assert __making == os.path.isdir(path), NO_MAKE_FOLDER_MSG
+    assert dircheck == __dir_none_making, FOUND_WRONG_FOLDER_MSG
+    assert __dir_making == os.path.isdir(path), NO_MAKE_FOLDER_MSG
 
 
 ###
@@ -94,8 +94,8 @@ def test_checkfile_search_name():
     __file = ".\\testdata\\testmp4.mp4"
 
     # 실행시켰을 때 결과
-    name = "testmp4.mp4"
-    file = check_file(name)
+    filename = "testmp4.mp4"
+    file = check_file(filename)
 
     # 결과 비교
     FILE_NOT_FOUND_MSG = f"Couldn't search file name {file}"
@@ -105,26 +105,24 @@ def test_checkfile_search_name():
 ###
 # test search_file
 ###
-def test_searchfile_one():
+def test_searchfile_one_file():
     """
     test searching the file if there was one file when it takes the file name
     파일이 하나만 있을 때(파일명 지정)
     """
     # 정상적으로 작동해야 하는 결과
-    __files = ".\\testdata\\testmp4.mp4"
+    __file = ".\\testdata\\testmp4.mp4"
 
     # 실행시켰을 때 결과
     directory = ".\\testdata"
-    files = search_file(
-        directory, filename="testmp4.mp4", recursive=True, extension=None
-    )
+    file = search_file(directory, filename="testmp4.mp4", recursive=True, extension=None)
 
     # 결과 비교
-    FILE_NOT_FOUND_MSG = f"File is not found : {files}"
-    assert __files == files, FILE_NOT_FOUND_MSG
+    FILE_NOT_FOUND_MSG = f"File is not found : {file}"
+    assert __file == file, FILE_NOT_FOUND_MSG
 
 
-def test_searchfile_list():
+def test_searchfile_files_list():
     """
     test searching the files if there were some files when it takes the file name
     파일이 여러 개 있을 때(파일명 지정)
@@ -141,13 +139,11 @@ def test_searchfile_list():
     files = search_file(directory, filename, recursive=True, extension=None)
 
     # 결과 비교
-    FOUND_WRONG_LIST_MSG = (
-        f"Couldn't read all file list about {filename} on {directory}"
-    )
+    FOUND_WRONG_LIST_MSG = f"Couldn't read all file list about {filename} on {directory}"
     assert __files == files, FOUND_WRONG_LIST_MSG
 
 
-def test_searchfile_all():
+def test_searchfile_all_files():
     """
     test returning the all files list (include subfolders' one)
     폴더 안 모든 파일 리스트 출력(하위 폴더 포함)
@@ -168,13 +164,11 @@ def test_searchfile_all():
     files = search_file(directory, filename=None, recursive=True, extension=None)
 
     # 결과 비교
-    FOUND_WRONG_LIST_MSG = (
-        f"Couldn't read all file list on {directory} (include subfolders)"
-    )
+    FOUND_WRONG_LIST_MSG = f"Couldn't read all file list on {directory} (include subfolders)"
     assert __files == files, FOUND_WRONG_LIST_MSG
 
 
-def test_searchfile_mp4():
+def test_searchfile_mp4_files():
     """
     test returning the all .mp4 files list (include subfolders' one)
     폴더 안 모든 mp4 파일 리스트 출력(하위 폴더 포함)
