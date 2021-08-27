3059fd5a65e7b8a3e0c3535c3a50677e8b84d26a
Kang Changgu <50660684+rxdcxdrnine@users.noreply.github.com>
2021-08-27 21:08:04 +0900

    Update server/backend/utils/validate.js
    fix typo with handal85's suggestion.
    
    Co-authored-by: SangilHan <45425838+handal95@users.noreply.github.com>

--- server/backend/utils/validate.js
+++ server/backend/utils/validate.js
@@ -12,7 +12,7 @@ export const validateDate = async (keyword) => {
   const regexpr = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/;
 
   if (!keyword.match(regexpr)) {
-    const err = new Error("Ivalid search keyword.");
+    const err = new Error("Invalid search keyword.");
     err.name = "SearchKeywordError";
     throw err;
   }
