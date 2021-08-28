58e81a98789043b59a58a730e82d9ccef8c444f5
owao <46257761+owao@users.noreply.github.com>
2021-08-28 16:59:34 +0900

    fix for async test

--- hub/tests/test_hub.py
+++ hub/tests/test_hub.py
@@ -21,20 +21,16 @@ async def test_run_anomaly_type():
 
     # 실행시켰을 때 결과
     path = ".\\tests\\hub_testdata"
-    (anomal, output) = VideoClient(path).run()
+    (anomal, output) = await VideoClient(path).run()
 
     # 결과 비교 (else 코드 TODO)
     if anomal == __anomal_assault:
         anomaly_type = output["anomaly_type"]
-        WRONG_ANOMALY_TYPE_MSG = (
-            f"Wrong anomaly type {anomaly_type} return ({__type_assault})"
-        )
+        WRONG_ANOMALY_TYPE_MSG = f"Wrong anomaly type {anomaly_type} return ({__type_assault})"
         assert anomaly_type == __type_assault, WRONG_ANOMALY_TYPE_MSG
     else:
         anomaly_type = output["anomaly_type"]
         ANOMAL_NOT_FOUND_MSG = "There is no anomal alert return"
-        RETURN_ANOTHER_ANOMAL_MSG = (
-            f"Found another anomal alert {anomal} ({__anomal_assault})"
-        )
+        RETURN_ANOTHER_ANOMAL_MSG = f"Found another anomal alert {anomal} ({__anomal_assault})"
         assert anomal != False, ANOMAL_NOT_FOUND_MSG
         assert anomaly_type == __type_assault, RETURN_ANOTHER_ANOMAL_MSG
